c31bcc508ae0105ac56e0b8e1aa6ea04731002fb
oddministrator <notlogic@gmail.com>
2026-06-15 23:46:27 +0000

    ui: simplify back button to just display logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,7 @@ function App() {
   return (
     <>
       <a href="https://radiological.info" className="return-link" title="Return to radiological.info">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M19 12H5M12 19l-7-7 7-7"/>
-        </svg>
         <img src={fullLogo} alt="Radiological Info Logo" className="return-logo" />
-        <span>Back</span>
       </a>
       {activeSet ? (
         <QuizScreen 
